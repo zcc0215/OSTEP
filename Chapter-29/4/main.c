2fdafbcca31e77287350af65f414a0bffcf3d71f
zcc0215 <zcc0215@gmail.com>
2021-09-22 11:39:38 +0800

    finish Chapter-29

---
+++ Chapter-29/4/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "linkList.h"
+#include "../timer.h"
+
+#define MAX_WORKER 8
+
+typedef struct {
+    list_t *list;
+} threadArgs;
+
+void *worker(void *tArgs) {
+    threadArgs *args = (threadArgs *) tArgs;
+    for (int i = 0; i < 10000; i++) {
+        list_insert(args->list, i);
+        int status = lookup(args->list, i);
+    }
+    return NULL;
+}
+
+int main() {
+    double start, end;
+    pthread_t ps[MAX_WORKER];
+    list_t list;
+    threadArgs tArgs;
+    for (int t = 1; t <= MAX_WORKER; t++) {
+        list_init(&list);
+        tArgs.list = &list;
+
+        start = Time_GetSeconds();
+
+        for (int i = 0; i < t; i++) {
+            Pthread_create(&ps[i], NULL, &worker, &tArgs);
+        }
+        for (int i = 0; i < t; i++) {
+            Pthread_join(ps[i], NULL);
+        }
+
+        end = Time_GetSeconds();
+        printf("%f\n", end - start);
+    }
+    free_list(&list);
+    return 0;
+}
---
+++ Chapter-29/timer.h
@@ -0,0 +1,13 @@
+#ifndef OPERATING_SYSTEMS_THREE_EASY_PIECES_NOTES_TIMER_H
+#define OPERATING_SYSTEMS_THREE_EASY_PIECES_NOTES_TIMER_H
+
+#include <assert.h>
+#include <sys/time.h>
+
+double Time_GetSeconds() {
+    struct timeval t;
+    assert(gettimeofday(&t, NULL) == 0);
+    return (double) ((double)t.tv_sec + (double)t.tv_usec / 1e6);
+}
+
+#endif
