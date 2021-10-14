36285360ea332e99f16452292db80526aff69161
zcc0215 <zcc0215@gmail.com>
2021-10-14 10:20:21 +0800

    finish P5a

--- Project/filesystems-checker/fs.h
+++ Project/filesystems-checker/fs.h
@@ -1,6 +1,6 @@
+#include "types.h"
 // On-disk file system format.
 // Both the kernel and user programs use this header file.
-
 
 #define ROOTINO 1  // root i-number
 #define BSIZE 512  // block size
